046742f03d437d5a4d7c011642bc0c6649daeef4
Ramon Barreras <lbarreras@nearsoft.com>
2016-11-23 09:16:44 -0700

    Improve test, Use spy.withArgs method

--- test/test.js
+++ test/test.js
@@ -8,15 +8,18 @@ describe('Array', function () {
 			//Setup
 			var array = [1, 2, 3];
 			var callback  = sinon.spy();
+			callback.withArgs(1,0);
+			callback.withArgs(2,1);
+			callback.withArgs(3,2);
 
 			//Execute
 			array.each(callback);
 
 			//Compare
 			assert.equal(callback.callCount, 3);
-			assert.deepEqual(callback.getCall(0).args, [1,0]);
-			assert.deepEqual(callback.getCall(1).args, [2,1]);
-			assert.deepEqual(callback.getCall(2).args, [3,2]);
+			assert.isTrue(callback.withArgs(1,0).calledOnce);
+			assert.isTrue(callback.withArgs(2,1).calledOnce);
+			assert.isTrue(callback.withArgs(3,2).calledOnce);
 		});
 	});
 });
