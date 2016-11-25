5e5995a76434d78b0899c09c6775074df7f1cbcb
Ramon Barreras <lbarreras@nearsoft.com>
2016-11-25 14:56:24 -0700

    Test Select in separate file

--- test/test.js
+++ test/test.js
@@ -23,30 +23,4 @@ describe('Array #each', function () {
 		assert.isTrue(callback.withArgs(4,3).calledOnce);
 		assert.isTrue(callback.withArgs(5,4).calledOnce);
 	});
-
-	describe('#select', function() {
-		it('Should create a new array with the output of the callback', function() {
-			//Setup
-			var people = [
-				{ name: 'juan', age: 15 },
-				{ name: 'pablo', age: 16 },
-				{ name: 'pedro', age: 19 }
-	    ];
-			var callback = sinon.stub();
-			callback.withArgs({ name: 'juan', age: 15 }).returns('juan');
-			callback.withArgs({ name: 'pablo', age: 16 }).returns('pablo');
-			callback.withArgs({ name: 'pedro', age: 19 }).returns('pedro');
-
-			//Execution
-			var response = people.select(callback);
-
-			//Compare
-			assert.deepEqual(response, ['juan', 'pablo', 'pedro']);
-			assert.equal(callback.callCount, 3);
-			assert.isTrue(callback.withArgs({ name: 'juan', age: 15 }).calledOnce);
-			assert.isTrue(callback.withArgs({ name: 'pablo', age: 16 }).calledOnce);
-			assert.isTrue(callback.withArgs({ name: 'pedro', age: 19 }).calledOnce);
-
-		});
-	});
 });
